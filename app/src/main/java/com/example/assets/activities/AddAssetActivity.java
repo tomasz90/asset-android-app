d1fb1e912ba9cade45646e0f06350e54ce6d6441
tomasz90 <tkulig@ptc.com>
2020-03-31 12:19:04 +0200

    In progress.

--- app/src/main/java/com/example/assets/activities/AddAssetActivity.java
+++ app/src/main/java/com/example/assets/activities/AddAssetActivity.java
@@ -1,6 +1,7 @@
 package com.example.assets.activities;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -14,6 +15,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.assets.R;
 
 public class AddAssetActivity extends AppCompatActivity {
+
+    TextView valueLabel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +26,7 @@ public class AddAssetActivity extends AppCompatActivity {
         String assetSymbol = getIntent().getStringExtra("asset");
         setAssetSymbol(assetSymbol);
 
-        TextView valueLabel = findViewById(R.id.calculated_value);
+        valueLabel = findViewById(R.id.calculated_value);
 
         Button save = findViewById(R.id.fab);
 
@@ -34,24 +38,25 @@ public class AddAssetActivity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                save.setBackgroundColor(getColor(R.color.magenta));
-                save.setEnabled(true);
+//                save.setBackgroundColor(getColor(R.color.magenta));
+//                save.setEnabled(true);
             }
 
             @Override
             public void afterTextChanged(Editable s) {
-                String textLabel;
-                float value = 0f;
-                if (s.toString().isEmpty()) {
-                    save.setBackgroundColor(getColor(R.color.greyed_magenta));
-                    save.setEnabled(false);
-                } else {
-                    value = Float.parseFloat(s.toString()) * getRate(assetSymbol);
-                }
-                textLabel = getString(R.string.calculated_value, value);
-                valueLabel.setText(textLabel);
+//                String textLabel;
+//                float value = 0f;
+//                if (s.toString().isEmpty()) {
+//                    save.setBackgroundColor(getColor(R.color.greyed_magenta));
+//                    save.setEnabled(false);
+//                } else {
+//                    value = Float.parseFloat(s.toString()) * getRate(assetSymbol);
+//                }
+//                textLabel = getString(R.string.calculated_value, value);
+//                valueLabel.setText(textLabel);
             }
         });
+        new GetJSONTask().execute();
 
         save.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,5 +74,28 @@ public class AddAssetActivity extends AppCompatActivity {
 
     private float getRate(String assetSymbol) {
         return 1f;
+    }
+
+    private class GetJSONTask extends AsyncTask<String, Void, String> {
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return CurrencyService.getRate();
+            } catch (Exception e) {
+                return "Unable to retrieve data. URL may be invalid.";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            valueLabel.setText(result);
+        }
     }
 }
--- app/src/main/java/com/example/assets/activities/MainActivity.java
+++ app/src/main/java/com/example/assets/activities/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends AbstractListActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, ActivityExample.class);
+                Intent intent = new Intent(MainActivity.this, AddAssetListActivity.class);
                 startActivity(intent);
             }
         });
