d54ee542defcc7cd4bbf4b331560584f8d1a018e
tomasz90 <tkulig@ptc.com>
2020-05-19 22:19:41 +0200

    4 tests working.

---
+++ app/src/androidTest/java/com/example/assets/Condition.java
@@ -0,0 +1,63 @@
+package com.example.assets;
+
+import android.app.Activity;
+
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.test.runner.lifecycle.ActivityLifecycleMonitorRegistry;
+
+import com.azimolabs.conditionwatcher.Instruction;
+import com.example.assets.activities.activities.MainActivity;
+
+import java.util.Collection;
+import java.util.Objects;
+
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+import static androidx.test.runner.lifecycle.Stage.RESUMED;
+
+class Condition {
+
+    public static class LoadingMainActivity extends Instruction {
+        @Override
+        public String getDescription() {
+            return "wait until MainActivity loaded";
+        }
+
+        @Override
+        public boolean checkCondition() {
+            Activity activity = getActivityInstance();
+            return activity instanceof MainActivity;
+        }
+    }
+
+    public static class LoadingListItems extends Instruction {
+
+        LoadingListItems(RecyclerView recyclerView) {
+            this.recyclerView = recyclerView;
+        }
+
+        private RecyclerView recyclerView;
+
+        @Override
+        public String getDescription() {
+            return "wait until list loaded";
+        }
+
+        @Override
+        public boolean checkCondition() {
+            return Objects.requireNonNull(recyclerView.getAdapter()).getItemCount() != 0;
+        }
+    }
+
+    private static Activity currentActivity = null;
+
+    private static Activity getActivityInstance(){
+        getInstrumentation().runOnMainSync(() -> {
+            Collection<Activity> resumedActivities =
+                    ActivityLifecycleMonitorRegistry.getInstance().getActivitiesInStage(RESUMED);
+            if (resumedActivities.iterator().hasNext()){
+                currentActivity = resumedActivities.iterator().next();
+            }
+        });
+        return currentActivity;
+    }
+}
--- app/src/androidTest/java/com/example/assets/MainActivityTest.java
+++ app/src/androidTest/java/com/example/assets/MainActivityTest.java
@@ -3,13 +3,17 @@ package com.example.assets;
 import android.app.Application;
 
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.test.espresso.action.ViewActions;
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
 import androidx.test.rule.ActivityTestRule;
 
+import com.azimolabs.conditionwatcher.ConditionWatcher;
 import com.example.assets.activities.activities.MainActivity;
+import com.example.assets.activities.list_adapters.AssetDetailsAdapter;
 import com.example.assets.storage.room.AssetDataBase;
 import com.example.assets.storage.room.entity.Asset;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Rule;
@@ -20,7 +24,9 @@ import java.util.Objects;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static androidx.test.espresso.action.ViewActions.clearText;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.assertThat;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
@@ -30,19 +36,33 @@ import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static com.example.assets.constants.AssetConstants.CURRENCIES;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(AndroidJUnit4ClassRunner.class)
 public class MainActivityTest {
+
+    private Condition.LoadingListItems listLoaded;
 
     @Rule
     public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Before
-    public void prepareTestData() {
+    public void prepareTest() throws Exception {
         Application application = mainActivityActivityTestRule.getActivity().getApplication();
         AssetDataBase dataBase = AssetDataBase.getInstance(application);
         Asset testAsset = new Asset("PLN", CURRENCIES, 150f, "info");
         dataBase.assetDao().insert(testAsset);
+
+        RecyclerView recyclerView = mainActivityActivityTestRule.getActivity().findViewById(R.id.asset_list);
+        listLoaded = new Condition.LoadingListItems(recyclerView);
+        ConditionWatcher.waitForCondition(listLoaded);
+    }
+
+    @After
+    public void removeTestData() {
+        Application application = mainActivityActivityTestRule.getActivity().getApplication();
+        AssetDataBase dataBase = AssetDataBase.getInstance(application);
+        dataBase.assetDao().deleteAll();
     }
 
     @Test
@@ -54,19 +74,6 @@ public class MainActivityTest {
         // then
         onView(withId(R.id.generic_list))
         .check(matches(isDisplayed()));
-    }
-
-    @Test
-    @Ignore("isEnabled not working...")
-    public void should_not_be_able_to_click_remove_assets_when_no_assets() {
-        // given
-        openActionBarOverflowOrOptionsMenu(mainActivityActivityTestRule.getActivity());
-
-        // when
-        onView(withText(getString(R.string.remove_all_assets)))
-
-        // then
-        .check(matches(not(isEnabled())));
     }
 
     @Test
@@ -86,7 +93,58 @@ public class MainActivityTest {
         assertThat(actual, equalTo(expected));
     }
 
+    @Test
+    public void should_not_see_asset_when_remove_it_by_swiping_right() throws Exception {
+        // when
+        onView(withText("PLN")).perform(ViewActions.swipeRight());
+        onView(withText(getString(R.string.want_to_remove_asset))).check(matches(isDisplayed()));
+        onView(withText(getString(R.string.yes))).perform(click());
+
+        // then
+        int expected = 0;
+        RecyclerView recyclerView = mainActivityActivityTestRule.getActivity().findViewById(R.id.asset_list);
+        int actual = Objects.requireNonNull(recyclerView.getAdapter()).getItemCount();
+        assertThat(actual, equalTo(expected));
+    }
+
+    @Test
+    public void should_asset_has_new_value_when_asset_was_edited() throws Exception {
+        // given
+        String expectedQuote = "1234";
+
+        // when
+        onView(withText("PLN")).perform(ViewActions.swipeLeft());
+        onView(withId(R.id.amount_input))
+                .perform(clearText())
+                .perform(typeText(expectedQuote));
+        onView(withId(R.id.fab))
+                .perform(click());
+
+        // then
+        ConditionWatcher.waitForCondition(new Condition.LoadingMainActivity());
+        RecyclerView recyclerView = mainActivityActivityTestRule.getActivity().findViewById(R.id.asset_list);
+        ConditionWatcher.waitForCondition(listLoaded);
+        int actualQuote = (int)((AssetDetailsAdapter) recyclerView.getAdapter()).getAssetAtPosition(0).getQuantity();
+        assertEquals(Integer.parseInt(expectedQuote), actualQuote);
+
+    }
+
+    @Test
+    @Ignore
+    public void should_not_be_able_to_click_remove_assets_when_no_assets() {
+        // given
+        openActionBarOverflowOrOptionsMenu(mainActivityActivityTestRule.getActivity());
+
+        // when
+        onView(withText(getString(R.string.remove_all_assets)))
+
+        // then
+        .check(matches(not(isEnabled())));
+    }
+
     private String getString(int stringResource) {
         return mainActivityActivityTestRule.getActivity().getString(stringResource);
     }
+
+
 }
