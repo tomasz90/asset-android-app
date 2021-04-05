eb7e6cbe1049ec95bc5d2d46c532aadd09421422
tomas <tomaszkacperkulig@gmail.com>
2021-04-05 22:39:46 +0200

    In progress

--- app/src/main/java/com/example/assets/util/client/CryptoProviderApiClient.kt
+++ app/src/main/java/com/example/assets/util/client/CryptoProviderApiClient.kt
@@ -6,12 +6,13 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.Query
 
 interface CryptoProviderApiClient {
 
     @Headers("X-CMC_PRO_API_KEY: dfff4181-377d-4dfe-8b50-52245c63eb05")
-    @GET("v1/cryptocurrency/listings/latest?limit=400")
-    fun getRates(): Call<CryptoRatesResponse>
+    @GET("v1/cryptocurrency/listings/latest")
+    fun getRates(@Query("limit") limit: Int = 400): Call<CryptoRatesResponse>
 }
 
 class CryptoRatesResponse(@JsonProperty("data") val data: List<CryptoData>): RatesResponse {
--- app/src/test/java/com/example/assets/integration/AssetServicesTest.java
+++ app/src/test/java/com/example/assets/integration/AssetServicesTest.java
@@ -47,7 +47,7 @@ public class AssetServicesTest {
 
         RateFacade facade = new RateFacade();
 
-        facade.getMetals().filter().getRates().forEach(x->System.out.println(x.getSymbol()));
+        facade.getCryptos().getRates().forEach(x->System.out.println(x.getSymbol()));
     }
 
     private void shouldReturnRates(String assetsName, List<String> assetsList) throws Exception {
